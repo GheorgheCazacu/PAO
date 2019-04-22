87ffa79c8afae1f3df21426ceaf7af946d8b6c68
gheorghe.cazacu@icloud.com <qRV-BXk-hPX-h9T>
2019-04-22 22:28:12 +0300

    documents lab 09

--- Laborator-09-GUI/src/com/fmi/laborator09/Main.java
+++ Laborator-09-GUI/src/com/fmi/laborator09/Main.java
@@ -1,11 +1,16 @@
 package com.fmi.laborator09;
 
 
+import com.fmi.laborator09.services.RandomGeneratorService;
+import com.fmi.laborator09.services.UserService;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("all the interesting swing components are in package com.fmi.laborator09.components!");
+
+        UserService.getInstance().findAll().forEach(System.out::println);
 
     }
 }
